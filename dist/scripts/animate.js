7f4e86f9a87d539fe3340c38650c02cf4ad779cc
Debajit Paul <62774980+Debajit-Paul@users.noreply.github.com>
2024-06-25 14:22:44 +0530

    corporate style and animation

--- dist/scripts/animate.js
+++ dist/scripts/animate.js
@@ -16,6 +16,7 @@ gsap.to("#match_skill", {
   duration: 3,
 });
 
+// stress_free
 gsap.to("#stress_free", {
   scrollTrigger: {
     trigger: "#stress_free",
@@ -33,11 +34,66 @@ gsap.to("#stress_free", {
   duration: 3,
 });
 
+// trust_us
 gsap.to("#trust_us", {
   scrollTrigger: {
     trigger: "#trust_us",
     start: "0% 100%",
-    end: "100% 70%",
+    end: "100% 80%",
+    // markers: true,
+    scrub: 1,
+  },
+  startAt: {
+    opacity: 0,
+    scale: 0,
+  },
+  opacity: 1,
+  scale: 1,
+  duration: 4,
+});
+
+// launchpad
+gsap.to("#launchpad", {
+  scrollTrigger: {
+    trigger: "#launchpad",
+    start: "0% 100%",
+    end: "100% 80%",
+    // markers: true,
+    scrub: 1,
+  },
+  startAt: {
+    opacity: 0,
+    scale: 0,
+  },
+  opacity: 1,
+  scale: 1,
+  duration: 4,
+});
+
+// top_notch
+gsap.to("#top_notch", {
+  scrollTrigger: {
+    trigger: "#top_notch",
+    start: "0% 100%",
+    end: "100% 80%",
+    // markers: true,
+    scrub: 1,
+  },
+  startAt: {
+    opacity: 0,
+    scale: 0,
+  },
+  opacity: 1,
+  scale: 1,
+  duration: 4,
+});
+
+// why_us
+gsap.to("#why_us", {
+  scrollTrigger: {
+    trigger: "#why_us",
+    start: "0% 100%",
+    end: "100% 80%",
     // markers: true,
     scrub: 1,
   },
